8846ab150ed51ce41e59bda89c4feb12b56d7810
croatialu <wuli.croatia@foxmail.com>
2022-08-05 22:52:51 +0800

    chore: 引入缓存

--- src/common/app-config/controller/app-config.controller.ts
+++ src/common/app-config/controller/app-config.controller.ts
@@ -1,10 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppConfigService } from '../service/app-config.service';
 
 @Controller('app-config')
 export class UserController {
   constructor(private readonly appConfigService: AppConfigService) {}
-
-  @Get('error-code')
-  getResponseErrorCode() {}
 }
